5084612d1a2676b110ce9c653094787a0775967b
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-03 17:17:33 +0300

    Added locators for TextBox page

---
+++ locators/elements_page_locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+
+class TextBoxPageLocators:
+    # Locators for form field
+    FULL_NAME = (By.ID, "userName")
+    EMAIL = (By.ID, "userEmail")
+    CURRENT_ADDRESS = (By.ID, "currentAddress")
+    PERNAMENT_ADDRESS = (By.ID, "permanentAddress")
+    SUBMIT_BUTTON = (By.ID, "submit")
+
+    # Locators for form validation
+    CREATED_FULL_NAME = (By.ID, "name")
+    CREATED_EMAIL = (By.ID, "email")
+    CREATED_CURRENT_ADDRESS = (By.ID, "currentAddress")
+    CREATED_PERNAMENT_ADDRESS = (By.ID, "permanentAddress")
