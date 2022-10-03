a6575d59800d32b3a90544d7a05506b5cd6c5c14
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-03 22:44:20 +0300

    Added missing line

--- conftest.py
+++ conftest.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
+
 @pytest.fixture
 def driver():
     chrome_options = ChromeOptions()
