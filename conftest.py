f13845260067b49eda50413c044b133994724d8f
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-03 18:28:09 +0300

    Fixed DeprecationWarning: executable_path has been deprecated.

--- conftest.py
+++ conftest.py
@@ -2,13 +2,13 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture
 def driver():
     chrome_options = ChromeOptions()
     chrome_options.add_argument("headless")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     yield driver
     driver.quit()
