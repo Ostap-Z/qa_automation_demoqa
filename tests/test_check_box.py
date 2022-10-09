8f2abd7500fdf75a99334a5c262ff18e04ee0674
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-09 19:47:40 +0300

    Add assertion in test_check_box file that verify input and output data equals

--- tests/test_check_box.py
+++ tests/test_check_box.py
@@ -8,3 +8,7 @@ class TestCheckBox:
         check_box_page.open()
         check_box_page.open_full_list()
         check_box_page.click_random_check_box()
+        input_checkbox = check_box_page.get_checked_checkboxes()
+        output_checkbox = check_box_page.get_output_result()
+        assert input_checkbox == output_checkbox, f"\nActual result: {output_checkbox}"\
+                                                                  f"\nExpected result: {input_checkbox}\n"
