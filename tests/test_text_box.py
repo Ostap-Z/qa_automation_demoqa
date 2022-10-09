e121025eb19ee3de8bb96378f104d117363732d7
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-09 16:27:05 +0300

    Add randomly generated person data by Faker for tests

--- tests/test_text_box.py
+++ tests/test_text_box.py
@@ -6,9 +6,7 @@ class TestTextBox:
     def test_fill_in_fields(self, driver):
         text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
         text_box_page.open()
-        text_box_page.fill_in_form_fields()
-        output_name, output_email, output_current_addr, output_pernament_addr = text_box_page.validate_filled_form()
-        print(output_name)
-        print(output_email)
-        print(output_current_addr)
-        print(output_pernament_addr)
+        input_data = text_box_page.fill_in_form_fields()
+        output_data = text_box_page.validate_filled_form()
+        assert input_data == output_data, f"\nActual output data: {output_data}\n"\
+                                          f"Expected data: {input_data}\n"
