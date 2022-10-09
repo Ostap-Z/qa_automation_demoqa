3942c8727b3ae445c3d4b152ac2038f05059e400
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-09 18:12:58 +0300

    Add function that opens full list of elements and rondomly clicks on it 10 times

--- tests/test_check_box.py
+++ tests/test_check_box.py
@@ -6,3 +6,5 @@ class TestCheckBox:
     def test_check_box(self, driver):
         check_box_page = CheckBoxPage(driver, "https://demoqa.com/checkbox")
         check_box_page.open()
+        check_box_page.open_full_list()
+        check_box_page.click_random_check_box()
