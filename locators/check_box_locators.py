3d29b1f5c78f1ff3c217a1c3c5cfd4505eb35be9
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-09 17:21:35 +0300

    Add locators for expand all button and check box items

--- locators/check_box_locators.py
+++ locators/check_box_locators.py
@@ -2,4 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class CheckBoxPageLocators:
-    pass
+
+    EXPAND_ALL_BUTTON = (By.CSS_SELECTOR, "button[title='Expand all']")
+    CHECK_BOXES_ITEM_LIST = (By.CLASS_NAME, "rct-title")
