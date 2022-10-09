05e900f0332df46163f9e31795aeb5a63a535be2
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-09 19:43:15 +0300

    Add locators: CHECKED_ITEMS_LIST, TITLE_ITEM, OUTPUT_RESULT

--- locators/check_box_locators.py
+++ locators/check_box_locators.py
@@ -5,3 +5,6 @@ class CheckBoxPageLocators:
 
     EXPAND_ALL_BUTTON = (By.CSS_SELECTOR, "button[title='Expand all']")
     CHECK_BOXES_ITEM_LIST = (By.CLASS_NAME, "rct-title")
+    CHECKED_ITEMS_LIST = (By.CLASS_NAME, "rct-icon-check")
+    TITLE_ITEM = (By.XPATH, ".//ancestor::span[@class='rct-text']")
+    OUTPUT_RESULT = (By.CLASS_NAME, "text-success")
