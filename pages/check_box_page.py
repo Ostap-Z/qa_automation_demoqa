99c7e22eda2c4d17f49ee8f345fccaaa6ddeda0e
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-09 18:11:12 +0300

    Add open_full_list function that opens all checkbox elements. Add click_random_check_box function that 10 times clicks on the random element

--- pages/check_box_page.py
+++ pages/check_box_page.py
@@ -1,6 +1,25 @@
-from base_page import BasePage
+from random import randint
+
+from pages.base_page import BasePage
 from locators.check_box_locators import CheckBoxPageLocators
 
 
 class CheckBoxPage(BasePage):
     locators = CheckBoxPageLocators()
+
+    def open_full_list(self):
+        self.element_is_visible(self.locators.EXPAND_ALL_BUTTON).click()
+
+    def click_random_check_box(self):
+        item_list = self.elements_are_visible(self.locators.CHECK_BOXES_ITEM_LIST)
+
+        count = 10
+        while count != 0:
+            item = item_list[randint(1, 5)]
+            if count > 0:
+                self.go_to_element(item)
+                item.click()
+                print(item.text)
+                count -= 1
+            else:
+                break
