a8d2ca4b622e9af0c77041d7137433b5c67fad1e
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-10 19:12:44 +0300

    Refactor locators for radio buttons

--- locators/radio_button_locators.py
+++ locators/radio_button_locators.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class RadioButtonPageLocators:
-    RADIO_BUTTONS_LIST = (By.CLASS_NAME, "custom-radio")
-    TITLE_ITEM = (By.XPATH, ".//ancestor::label[@class='custom-control-label']")
-    OUTPUT_RESULT = (By.CLASS_NAME, "text-success")
+    RADIOBUTTON_YES = (By.CSS_SELECTOR, "label[class^='custom-control'][for='yesRadio']")
+    RADIOBUTTON_IMPRESSIVE = (By.CSS_SELECTOR, "label[class^='custom-control'][for='impressiveRadio']")
+    RADIOBUTTON_NO = (By.CSS_SELECTOR, "label[class^='custom-control'][for='noRadio']")
+    OUTPUT_RESULT = (By.CSS_SELECTOR, "p > span[class='text-success']")
