eba604ef09293f6683e03aad25c51245c868daa5
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-10 17:54:42 +0300

    Replaced old name of variables with a new one in text_box_page

--- pages/text_box_page.py
+++ pages/text_box_page.py
@@ -16,13 +16,13 @@ class TextBoxPage(BasePage):
         self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
         self.element_is_visible(self.locators.EMAIL).send_keys(email)
         self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
-        self.element_is_visible(self.locators.PERNAMENT_ADDRESS).send_keys(permanent_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT_BUTTON).click()
-        return full_name,email, current_address, permanent_address
+        return full_name, email, current_address, permanent_address
 
     def validate_filled_form(self):
         full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(":")[1]
         email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(":")[1]
         current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text
-        permanent_address = self.element_is_present(self.locators.CREATED_PERNAMENT_ADDRESS).text
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text
         return full_name, email, current_address, permanent_address
