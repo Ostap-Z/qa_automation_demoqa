6925547081517cf1dafdc11baf2d59fb2661df09
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-11 14:55:18 +0300

    Add locators for registration form

---
+++ locators/web_table_locators.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+class WebTablePageLocators:
+    """Locators for the registration a new user """
+    ADD_BUTTON = (By.ID, "addNewRecordButton")
+    FIRST_NAME_INPUT = (By.ID, "firstName")
+    LAST_NAME_INPUT = (By.ID, "lastName")
+    EMAIL_INPUT = (By.ID, "userEmail")
+    AGE_INPUT = (By.ID, "age")
+    SALARY_INPUT = (By.ID, "salary")
+    DEPARTMENT_INPUT = (By.ID, "department")
+    SUBMIT_BUTTON = (By.ID, "submit")
