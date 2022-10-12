0cb3bbcd1d69f997d2e0b88793d190bdeac2fcbe
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-12 17:46:31 +0300

    Added update button to the table locators

--- locators/web_table_locators.py
+++ locators/web_table_locators.py
@@ -15,6 +15,7 @@ class WebTablePageLocators:
     # Locators for the table
     FULL_PERSON_LIST = (By.CSS_SELECTOR, "div.rt-tr-group")
     DELETE_BUTTON = (By.CSS_SELECTOR, "span[title='Delete']")
+    UPDATE_BUTTON = (By.CSS_SELECTOR, "span[title='Edit']")
     ROW_PARENT = (By.XPATH, ".//ancestor::div[@class='rt-tr-group']")
 
     # Locator for the search
