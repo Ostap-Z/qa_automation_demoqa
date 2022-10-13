5e0d2f20608666d74a26d571eaa1f4337f696318
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-13 13:07:47 +0300

    Added logs for radio button tests

--- pages/radio_button_page.py
+++ pages/radio_button_page.py
@@ -6,11 +6,16 @@ class RadioButtonPage(BasePage):
     locators = RadioButtonPageLocators()
 
     def click_on_the_radio_button(self, choice):
+        log = self.get_logger()
         choices = {'yes': self.locators.RADIOBUTTON_YES,
                    'impressive': self.locators.RADIOBUTTON_IMPRESSIVE,
                    'no': self.locators.RADIOBUTTON_NO}
 
         radio = self.element_is_visible(choices[choice]).click()
+        log.info(f"Clicked on the radio button: {choices[choice]}")
 
     def output_result(self):
-        return self.element_is_present(self.locators.OUTPUT_RESULT).text
+        log = self.get_logger()
+        result = self.element_is_present(self.locators.OUTPUT_RESULT).text
+        log.info(f"Clicked radio button displayed in the output result: {result}")
+        return result
