ba3a205c23b98a3d02fb6159c2fff8006b894cae
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-13 19:11:31 +0300

    Changed expansion for the logfile

--- pages/base_page.py
+++ pages/base_page.py
@@ -39,7 +39,7 @@ class BasePage:
     def get_logger():
         logger_name = inspect.stack()[1][3]
         logger = logging.getLogger(logger_name)
-        file_handler = logging.FileHandler('logfile.txt')
+        file_handler = logging.FileHandler('logfile.log')
         formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
