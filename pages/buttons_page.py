be2b988bb355805c783b60bde4bb2619143b50a4
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-17 23:29:42 +0300

    Added logs to the button tests

--- pages/buttons_page.py
+++ pages/buttons_page.py
@@ -6,17 +6,24 @@ class ButtonsPage(BasePage):
     locators = ButtonsPageLocators()
 
     def click_on_the_different_button(self, type_click):
+        log = self.get_logger()
         if type_click == "double":
             self.action_double_click(self.element_is_visible(self.locators.DOUBLE_CLICK_BUTTON))
+            log.info(f"Button {type_click} was pressed")
             return self.check_clicked_button(self.locators.SUCCESS_DOUBLE_CLICK)
 
         if type_click == "right":
             self.action_right_click(self.element_is_visible(self.locators.RIGHT_CLICK_BUTTON))
+            log.info(f"Button {type_click} was pressed")
             return self.check_clicked_button(self.locators.SUCCESS_RIGHT_CLICK)
 
         if type_click == "left":
             self.element_is_visible(self.locators.LEFT_CLICK_BUTTON).click()
+            log.info(f"Button {type_click} was pressed")
             return self.check_clicked_button(self.locators.SUCCESS_LEFT_CLICK)
 
     def check_clicked_button(self, element):
-        return self.element_is_present(element).text
+        log = self.get_logger()
+        result = self.element_is_present(element).text
+        log.info(f"Result text: {result}")
+        return result
