3e38ffed47bf3f4964cfe6fa49fd975cbfbb5ddd
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-18 13:17:22 +0300

    Added locators for simple and bad request links

---
+++ locators/links_locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
+class LinksPageLocators:
+    SIMPLE_LINK = (By.CSS_SELECTOR, "a#simpleLink")
+    BAD_REQUEST_LINK = (By.CSS_SELECTOR, "a#bad-request")
