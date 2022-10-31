722b90404767db1b504ec8e28b53c5d40c3aef6f
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-31 23:15:53 +0200

    Added locators for upload file tests

--- locators/upload_download_locators.py
+++ locators/upload_download_locators.py
@@ -2,4 +2,5 @@ from selenium.webdriver.common.by import By
 
 
 class UploadAndDownloadLocators:
-    pass
+    UPLOAD_FILE = (By.CSS_SELECTOR, "input#uploadFile")
+    UPLOADED_RESULT = (By.CSS_SELECTOR, "p#uploadedFilePath")
