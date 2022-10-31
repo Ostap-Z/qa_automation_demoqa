5caab715dcefd9d43d7056d9a12fd69011e1ad7c
Ostap-Z <zherebetskyiq7@gmail.com>
2022-10-31 23:18:28 +0200

    Added upload_file test that uploads a file and verifies that the file was successfully added

--- pages/upload_download_page.py
+++ pages/upload_download_page.py
@@ -1,8 +1,23 @@
+import os
+
 from pages.base_page import BasePage
 from locators.upload_download_locators import UploadAndDownloadLocators
+from generator.generator import generated_file
 
 
 class UploadAndDownloadPage(BasePage):
     locators = UploadAndDownloadLocators()
 
-    
+    def upload_file(self):
+        file_name, path = generated_file()
+        self.element_is_present(self.locators.UPLOAD_FILE).send_keys(path)
+        self.remove_file(path)
+        return file_name.split("\\")[-1], self.uploaded_result.split("\\")[-1]
+
+    @property
+    def uploaded_result(self):
+        return self.element_is_present(self.locators.UPLOADED_RESULT).text
+
+    @staticmethod
+    def remove_file(file):
+        os.remove(file)
--- tests/test_upload_download.py
+++ tests/test_upload_download.py
@@ -6,7 +6,6 @@ class TestUploadAndDownload:
     def test_upload_file(self, driver):
         upload_page = UploadAndDownloadPage(driver, "https://demoqa.com/upload-download")
         upload_page.open()
-
-    def test_download_file(self, driver):
-        download_page = UploadAndDownloadPage(driver, "https://demoqa.com/upload-download")
-        download_page.open()
+        file_name, result = upload_page.upload_file()
+        assert file_name == result, f"\nActual result: {result}" \
+                                    f"\nExpected result: {file_name}"
