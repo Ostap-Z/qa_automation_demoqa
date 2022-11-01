aa270d833011256a5f7cd3a08bde79b810675c2e
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-01 11:25:24 +0200

    Added logs to the upload tests

--- pages/upload_page.py
+++ pages/upload_page.py
@@ -9,14 +9,21 @@ class UploadPage(BasePage):
     locators = UploadLocators()
 
     def upload_file(self):
+        log = self.get_logger()
         file_name, path = generated_file()
+        log.info(f"Generated file: {file_name}")
         self.element_is_present(self.locators.UPLOAD_FILE).send_keys(path)
+        log.info(f"Uploaded a file {file_name}")
         self.remove_file(path)
+        log.info(f"Removed a file {file_name} from project directory")
         return file_name.split("\\")[-1], self.uploaded_result.split("\\")[-1]
 
     @property
     def uploaded_result(self):
-        return self.element_is_present(self.locators.UPLOADED_RESULT).text
+        log = self.get_logger()
+        result = self.element_is_present(self.locators.UPLOADED_RESULT).text.split("\\")[-1]
+        log.info(f'Uploaded file presented in text: {result}')
+        return result
 
     @staticmethod
     def remove_file(file):
