a2286d82ab809302e666a2146fc148ec1d27ac0b
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-02 16:22:23 +0200

    Added locators for dynamic properties

---
+++ locators/dynamic_properties_locators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+class DynamicPropertiesLocators:
+    ENABLE_AFTER_FIVE_SEC_BUTTON = (By.CSS_SELECTOR, "button#enableAfter")
+    COLOR_CHANGE_BUTTON = (By.CSS_SELECTOR, "button#colorChange")
+    VISIBLE_AFTER_FIVE_SEC_BUTTON = (By.CSS_SELECTOR, "button#visibleAfter")
+    TEXT_RANDOM_ID = (By.CSS_SELECTOR, "div[class='col-12 mt-4 col-md-6'] > div:nth-child(2) > p")
