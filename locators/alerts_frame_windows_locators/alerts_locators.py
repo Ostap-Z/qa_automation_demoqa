3cda9db25fe32d1e01fb1b52309fc23da0474184
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-05 15:51:26 +0200

    Added alerts locators for QA needs

---
+++ locators/alerts_frame_windows_locators/alerts_locators.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.common.by import By
+
+
+class AlertsLocators:
+    # Button locators
+    ALERT_BUTTON = (By.CSS_SELECTOR, "button#alertButton")
+    ALERT_TIMER_BUTTON = (By.CSS_SELECTOR, "button#timerAlertButton")
+    ALERT_CONFIRM_BUTTON = (By.CSS_SELECTOR, "button#confirmButton")
+    ALERT_PROMPT_BUTTON = (By.CSS_SELECTOR, "button#promtButton")
+
+    # Verification text results
+    CONFIRM_TEXT_RESULT = (By.CSS_SELECTOR, "span#confirmResult")
+    PROMPT_TEXT_RESULT = (By.CSS_SELECTOR, "span#promptResult")
