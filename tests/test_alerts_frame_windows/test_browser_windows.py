2c1616bca47886e8b40e5f5757a2793f040a00c8
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-05 15:33:25 +0200

    Added test_new_window that verifies text_result is correct on the new opened window

--- tests/test_alerts_frame_windows/test_browser_windows.py
+++ tests/test_alerts_frame_windows/test_browser_windows.py
@@ -11,3 +11,10 @@ class TestBrowserWindows:
         text_result = new_tab_page.check_opened_new_tab()
         assert text_result == "This is a sample page", f"Actual result: {text_result}" \
                                                        "Expected result: This is a sample page"
+
+    def test_new_window(self, driver):
+        new_window_page = BrowserWindowsPage(driver, "https://demoqa.com/browser-windows")
+        new_window_page.open()
+        text_result = new_window_page.check_opened_new_window()
+        assert text_result == "This is a sample page", f"Actual result: {text_result}" \
+                                                       "Expected result: This is a sample page"
