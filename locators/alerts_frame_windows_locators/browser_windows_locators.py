5902d210c9b17c11bfec947a20dfbc1d9e1e1348
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-05 15:07:03 +0200

    Removed NEW_WINDOW_MESSAGE_BUTTON locator due to it will not be tested

--- locators/alerts_frame_windows_locators/browser_windows_locators.py
+++ locators/alerts_frame_windows_locators/browser_windows_locators.py
@@ -4,6 +4,5 @@ from selenium.webdriver.common.by import By
 class BrowserWindowsLocators:
     NEW_TAB_BUTTON = (By.CSS_SELECTOR, "button#tabButton")
     NEW_WINDOW_BUTTON = (By.CSS_SELECTOR, "button#windowButton")
-    NEW_WINDOW_MESSAGE_BUTTON = (By.CSS_SELECTOR, "messageWindowButton")
 
     TITLE_NEW = (By.CSS_SELECTOR, "h1#sampleHeading")
