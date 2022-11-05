bd3f711ae0cc44297295b12925a0522cea72e94f
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-05 15:30:54 +0200

    Refactored check_opened_new_tab method. Added check_opened_new_window that returns a result text in the new opened window

--- pages/alerts_frame_windows_page/browser_windows_page.py
+++ pages/alerts_frame_windows_page/browser_windows_page.py
@@ -7,6 +7,12 @@ class BrowserWindowsPage(BasePage):
 
     def check_opened_new_tab(self):
         self.element_is_visible(self.locators.NEW_TAB_BUTTON).click()
-        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.go_to_new_window(1)
+        text_title = self.element_is_present(self.locators.TITLE_NEW).text
+        return text_title
+
+    def check_opened_new_window(self):
+        self.element_is_visible(self.locators.NEW_WINDOW_BUTTON).click()
+        self.go_to_new_window(1)
         text_title = self.element_is_present(self.locators.TITLE_NEW).text
         return text_title
