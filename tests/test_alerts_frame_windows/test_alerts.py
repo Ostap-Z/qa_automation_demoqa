efd314a80959b8d6e421e2d81c6b191dc36f28f2
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-06 16:03:11 +0200

    Added test_timer_alert that verifies alert is opened in 5 seconds and result text in the alert is equals to the expected

--- tests/test_alerts_frame_windows/test_alerts.py
+++ tests/test_alerts_frame_windows/test_alerts.py
@@ -9,3 +9,11 @@ class TestAlerts:
         alert_result = alert_page.check_default_alert()
         assert alert_result == "You clicked a button", f"\nActual result: alert is not presented. Alert text: {alert_result}" \
                                                        "\nExpected result: alert should be presented with text 'You clicked a button'"
+
+    def test_timer_alert(self, driver):
+        alert_page = AlertsPage(driver, "https://demoqa.com/alerts")
+        alert_page.open()
+        alert_result = alert_page.check_timer_alert()
+        assert alert_result == "This alert appeared after 5 seconds", \
+            f"\nActual result: alert is not presented. Alert text: {alert_result}" \
+            "\nExpected result: alert should be presented with text 'This alert appeared after 5 seconds'"
