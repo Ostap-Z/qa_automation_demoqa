7a38a6a264aafe66a203a630cd3d1eeec710b65a
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-07 15:36:03 +0200

    Added locators for nested frames QA needs

---
+++ locators/alerts_frame_windows_locators/nested_frames_locators.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+class NestedFramesLocators:
+    PARENT_FRAME = (By.CSS_SELECTOR, "iframe#frame1")
+    CHILD_FRAME = (By.CSS_SELECTOR, "iframe[srcdoc='<p>Child Iframe</p>']")
+    PARENT_TEXT = (By.XPATH, "//*[text()='Parent frame']")
+    CHILD_TEXT = (By.XPATH, "//p[text()='Child Iframe']")
