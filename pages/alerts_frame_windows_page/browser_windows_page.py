7c87fb9880adba93b8667667de497671341ddc90
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-07 13:21:57 +0200

    Added additional logs to the browser_windows_page

--- pages/alerts_frame_windows_page/browser_windows_page.py
+++ pages/alerts_frame_windows_page/browser_windows_page.py
@@ -6,13 +6,21 @@ class BrowserWindowsPage(BasePage):
     locators = BrowserWindowsLocators()
 
     def check_opened_new_tab(self):
+        log = self.get_logger()
         self.element_is_visible(self.locators.NEW_TAB_BUTTON).click()
+        log.info("Clicked on the new tab button")
         self.go_to_new_window(1)
+        log.info("Switched to the new tab")
         text_title = self.element_is_present(self.locators.TITLE_NEW).text
+        log.info(f"Text presented in the new tab: {text_title}")
         return text_title
 
     def check_opened_new_window(self):
+        log = self.get_logger()
         self.element_is_visible(self.locators.NEW_WINDOW_BUTTON).click()
+        log.info("Clicked on the new window button")
         self.go_to_new_window(1)
+        log.info("Switched to the new window")
         text_title = self.element_is_present(self.locators.TITLE_NEW).text
+        log.info(f"Text presented in the new window: {text_title}")
         return text_title
