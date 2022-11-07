5a9ba977c97357dbe869c022521786426a898c63
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-07 13:05:13 +0200

    Added additional logs to the alerts_page

--- pages/alerts_frame_windows_page/alerts_page.py
+++ pages/alerts_frame_windows_page/alerts_page.py
@@ -11,36 +11,59 @@ class AlertsPage(BasePage):
     locators = AlertsLocators()
 
     def check_default_alert(self):
+        log = self.get_logger()
         self.element_is_visible(self.locators.ALERT_BUTTON).click()
+        log.info("Clicked on the alert button")
         try:
             alert = self.go_to_alert()
+            log.info("Went to the alert")
+            log.info(f"Alert text is '{alert.text}'")
             return alert.text
         except UnexpectedAlertPresentException:
             alert = self.go_to_alert()
+            log.info("Went to the alert")
+            log.info(f"Alert text is '{alert.text}'")
             return alert.text
 
     def check_timer_alert(self):
+        log = self.get_logger()
         self.element_is_visible(self.locators.ALERT_TIMER_BUTTON).click()
+        log.info("Clicked on the timer alert button")
         time.sleep(5)
         try:
             alert = self.go_to_alert()
+            log.info("Went to the alert")
+            log.info(f"Alert text is '{alert.text}'")
             return alert.text
         except UnexpectedAlertPresentException:
             alert = self.go_to_alert()
+            log.info("Went to the alert")
+            log.info(f"Alert text is '{alert.text}'")
             return alert.text
 
     def check_confirm_alert(self):
+        log = self.get_logger()
         self.element_is_visible(self.locators.ALERT_CONFIRM_BUTTON).click()
+        log.info("Clicked on the confirm alert button")
         alert = self.go_to_alert()
+        log.info("Went to the alert")
         alert.accept()
+        log.info("Accepted alert")
         confirm_result_text = self.element_is_present(self.locators.CONFIRM_TEXT_RESULT).text
+        log.info(f"Text presented in the result: '{confirm_result_text.split(' ')[-1].lower()}'")
         return confirm_result_text.split(" ")[-1].lower()
 
     def check_prompt_alert(self):
+        log = self.get_logger()
         text = f"autotest{randint(0, 1000)}"
         self.element_is_visible(self.locators.ALERT_PROMPT_BUTTON).click()
+        log.info("Clicked on the prompt alert button")
         alert = self.go_to_alert()
+        log.info("Went to the alert")
         alert.send_keys(text)
+        log.info(f"Filled in alert prompt with data: '{text}'")
         alert.accept()
+        log.info("Accepted alert")
         prompt_text_result = self.element_is_present(self.locators.PROMPT_TEXT_RESULT).text
+        log.info(f"Text presented in the result: '{prompt_text_result.split(' ')[-1]}'")
         return prompt_text_result.split(" ")[-1], text
