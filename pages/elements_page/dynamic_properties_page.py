23dab161dd2f77ad383c4c771d249c79b34a7b8a
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-07 13:54:26 +0200

    Added additional logs to the dynamic_properties_page

--- pages/elements_page/dynamic_properties_page.py
+++ pages/elements_page/dynamic_properties_page.py
@@ -10,22 +10,30 @@ class DynamicPropertiesPage(BasePage):
     locators = DynamicPropertiesLocators()
 
     def check_enable_button(self):
+        log = self.get_logger()
         try:
             self.element_is_clickable(self.locators.ENABLE_AFTER_FIVE_SEC_BUTTON)
+            log.info("Clicked on the enable after 5 seconds button")
         except TimeoutException:
             return False
         return True
 
     def check_changed_color(self):
+        log = self.get_logger()
         color_button = self.element_is_present(self.locators.COLOR_CHANGE_BUTTON)
+        log.info("Color button is presented in the DOM")
         color_button_before = color_button.value_of_css_property("color")
+        log.info(f"Color button before: {color_button_before}")
         time.sleep(5)
         color_button_after = color_button.value_of_css_property("color")
+        log.info(f"Color button after: {color_button_after}")
         return color_button_before, color_button_after
 
     def check_appear_button(self):
+        log = self.get_logger()
         try:
             self.element_is_visible(self.locators.VISIBLE_AFTER_FIVE_SEC_BUTTON)
+            log.info("Visible after 5 seconds button is visible")
         except TimeoutException:
             return False
         return True
