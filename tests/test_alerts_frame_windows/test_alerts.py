1cf0578af8f620767b5827457ac30f90a78463a8
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-07 12:40:22 +0200

    Added test_prompt_alert that verifies sent value in the prompt alert is presented and equals to the expected result

--- tests/test_alerts_frame_windows/test_alerts.py
+++ tests/test_alerts_frame_windows/test_alerts.py
@@ -25,3 +25,10 @@ class TestAlerts:
         assert confirm_alert_result == "ok".lower(), \
             f"\nActual result: alert is not presented or confirmed, text result is {confirm_alert_result}"\
             "\nExpected result: message 'ok' should be shown after the alert confirmation"
+
+    def test_prompt_alert(self, driver):
+        alert_page = AlertsPage(driver, "https://demoqa.com/alerts")
+        alert_page.open()
+        prompt_text_result, text = alert_page.check_prompt_alert()
+        assert text == prompt_text_result, f"Actual result: text presented in the span is '{prompt_text_result}'"\
+                                           f"Expected result: result text in the span should be '{text}'"
