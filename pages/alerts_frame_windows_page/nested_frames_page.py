a5ddcc4ad344bdd7fe7dbdd475f2f23b7610fa5b
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-07 15:43:50 +0200

    Added additional logs to the nested_frames_page

--- pages/alerts_frame_windows_page/nested_frames_page.py
+++ pages/alerts_frame_windows_page/nested_frames_page.py
@@ -1,3 +1,5 @@
+import logging
+
 from pages.base_page import BasePage
 from locators.alerts_frame_windows_locators.nested_frames_locators import NestedFramesLocators
 
@@ -6,11 +8,19 @@ class NestedFramesPage(BasePage):
     locators = NestedFramesLocators()
 
     def check_nested_frame(self):
+        log = self.get_logger()
         parent_frame = self.element_is_present(self.locators.PARENT_FRAME)
+        log.info("Parent frame is presented in the DOM")
         self.go_to_frame(parent_frame)
+        log.info("Went to the parent frame")
         parent_text = self.element_is_present(self.locators.PARENT_TEXT).text
+        log.info(f"Taken parent text: '{parent_text}'")
 
         child_frame = self.element_is_present(self.locators.CHILD_FRAME)
+        log.info("Child frame is presented in the DOM")
         self.go_to_frame(child_frame)
+        log.info("Went to the child frame")
         child_text = self.element_is_present(self.locators.CHILD_TEXT).text
+        log.info(f"Taken child text: '{child_text}'")
+        log.info(f"Returned texts for parent_frame, child_frame: '{parent_text}', '{child_text}'")
         return parent_text, child_text
