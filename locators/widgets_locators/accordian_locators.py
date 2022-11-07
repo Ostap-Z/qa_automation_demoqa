06c5f930e67b45b8cf979d3ba1fffddff2f0a294
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-07 22:27:35 +0200

    Added accordian locators for QA needs

---
+++ locators/widgets_locators/accordian_locators.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+
+class AccordianLocators:
+    FIRST_HEADING = (By.CSS_SELECTOR, "div#section1Heading")
+    FIRST_CONTENT = (By.CSS_SELECTOR, "div#section1Content > p")
+
+    SECOND_HEADING = (By.CSS_SELECTOR, "div#section2Heading")
+    SECOND_CONTENT = (By.CSS_SELECTOR, "div#section2Content > p")
+
+    THIRD_HEADING = (By.CSS_SELECTOR, "div#section3Heading")
+    THIRD_CONTENT = (By.CSS_SELECTOR, "div#section3Content > p")
