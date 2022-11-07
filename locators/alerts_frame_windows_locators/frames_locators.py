f68ee43212ae0eb31c84637f123d571cc081de4f
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-07 14:41:58 +0200

    Added locators for frames QA needs

---
+++ locators/alerts_frame_windows_locators/frames_locators.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+
+class FramesLocators:
+    FIRST_FRAME = (By.CSS_SELECTOR, "iframe#frame1")
+    SECOND_FRAME = (By.CSS_SELECTOR, "iframe#frame2")
+    TITLE_FRAME = (By.CSS_SELECTOR, "h1#sampleHeading")
