3decd0dac12c101b31ba6d6c5ca147dd31d30f33
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-08 17:28:24 +0200

    Added tabs locators for QA needs

---
+++ locators/widgets_locators/tabs_locators.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+
+class TabsLocators:
+    WHAT_TAB = (By.CSS_SELECTOR, "a#demo-tab-what")
+    WHAT_CONTENT = (By.CSS_SELECTOR, "div#demo-tabpane-what > p")
+
+    ORIGIN_TAB = (By.CSS_SELECTOR, "a#demo-tab-origin")
+    ORIGIN_CONTENT = (By.CSS_SELECTOR, "div#demo-tabpane-origin > p")
+
+    USE_TAB = (By.CSS_SELECTOR, "a#demo-tab-use")
+    USE_CONTENT = (By.CSS_SELECTOR, "div#demo-tabpane-use > p")
+
+    MORE_TAB = (By.CSS_SELECTOR, "demo-tab-more")
+    MORE_CONTENT = (By.CSS_SELECTOR, "div#demo-tabpane-more")
