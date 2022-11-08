72da3a55ae80b13015d986e80b42a183029063c3
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-08 16:45:49 +0200

    Refactored variable names in the slider tests

--- tests/test_widgets/test_slider.py
+++ tests/test_widgets/test_slider.py
@@ -6,7 +6,7 @@ class TestSlider:
     def test_slider(self, driver):
         slider_page = SliderPage(driver, "https://demoqa.com/slider")
         slider_page.open()
-        before, after = slider_page.change_slider_value()
-        assert before != after, \
-            f"\nActual result:\n\tSlider was not dragged.\n\tBefore position: {before}\n\tAfter position: {after}"\
+        slider_value_before, slider_value_after = slider_page.change_slider_value()
+        assert slider_value_before != slider_value_after, \
+            f"\nActual result:\n\tSlider was not dragged.\n\tBefore position: {slider_value_before}\n\tAfter position: {slider_value_after}"\
             "\nExpected result:\n\tThe slider should be dragged. So, after and before positions should not be the same"
