b7b0ca2f2833473dfddac3d360c64a2ed8338b4c
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-08 16:52:12 +0200

    Added progress bar locators for QA needs

---
+++ locators/widgets_locators/progress_bar_locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
+class ProgressBarLocators:
+    PROGRESS_BAR_BUTTON = (By.CSS_SELECTOR, "button#startStopButton")
+    PROGRESS_BAR_VALUE = (By.CSS_SELECTOR, "div.progress-bar.bg-info")
