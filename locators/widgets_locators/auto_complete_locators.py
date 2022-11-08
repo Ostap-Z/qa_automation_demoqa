c3b27a8480423c31884d9d9494743f160ef2078f
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-08 13:18:42 +0200

    Added single autocomplete locators for QA needs

--- locators/widgets_locators/auto_complete_locators.py
+++ locators/widgets_locators/auto_complete_locators.py
@@ -5,3 +5,6 @@ class AutoCompleteLocators:
     MULTIPLE_INPUT = (By.CSS_SELECTOR, "input#autoCompleteMultipleInput")
     MULTIPLE_VALUE = (By.CSS_SELECTOR, "div.css-1rhbuit-multiValue.auto-complete__multi-value")
     MULTIPLE_VALUE_REMOVE = (By.CSS_SELECTOR, "div.css-1rhbuit-multiValue.auto-complete__multi-value svg > path")
+
+    SINGLE_INPUT = (By.CSS_SELECTOR, "input#autoCompleteSingleInput")
+    SINGLE_VALUE = (By.CSS_SELECTOR, "div.auto-complete__single-value.css-1uccc91-singleValue")
