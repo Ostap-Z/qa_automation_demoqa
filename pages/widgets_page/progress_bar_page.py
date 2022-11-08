4ae39cb9eb3e6f6cc02c93a0a77c631c9694d9b9
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-08 17:09:22 +0200

    Added additional logs to the progress_bar_page

--- pages/widgets_page/progress_bar_page.py
+++ pages/widgets_page/progress_bar_page.py
@@ -9,10 +9,14 @@ class ProgressBarPage(BasePage):
     locators = ProgressBarLocators()
 
     def change_progress_bar_value(self):
+        log = self.get_logger()
         value_before = self.element_is_present(self.locators.PROGRESS_BAR_VALUE).get_attribute("aria-valuenow")
-        progress_bar_button = self.element_is_visible(self.locators.PROGRESS_BAR_BUTTON)
+        log.info(f"Progress bar value before: {value_before}")
+        progress_bar_button = self.element_is_present(self.locators.PROGRESS_BAR_BUTTON)
         progress_bar_button.click()
-        sleep(randint(0, 13))
+        sleep(randint(0, 7))
+        log.info("Progress bar was moved")
         progress_bar_button.click()
         value_after = self.element_is_present(self.locators.PROGRESS_BAR_VALUE).get_attribute("aria-valuenow")
+        log.info(f"Progress bar value after: {value_after}")
         return value_before, value_after
