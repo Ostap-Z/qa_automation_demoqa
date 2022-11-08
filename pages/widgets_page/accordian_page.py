5d0a77b305a20735324d6450ad9aca72b421b690
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-08 12:29:59 +0200

    Added additional logs to the accordian page

--- pages/widgets_page/accordian_page.py
+++ pages/widgets_page/accordian_page.py
@@ -8,6 +8,7 @@ class AccordianPage(BasePage):
     locators = AccordianLocators()
 
     def check_accordian(self, accordian_num):
+        log = self.get_logger()
         accordian = {
             'first': {
                 'title': self.locators.FIRST_HEADING,
@@ -24,18 +25,29 @@ class AccordianPage(BasePage):
         }
         self.hide_ads()
         accordian_title = self.element_is_visible(accordian[accordian_num]['title'])
+        log.info("Accordian title is visible on the page")
         accordian_title.click()
+        log.info("Opened accordian title section")
         try:
             accordian_content = self.element_is_visible(accordian[accordian_num]['content']).text
+            log.info(f"Get accordian content: {accordian_content}")
         except TimeoutException:
             accordian_title.click()
+            log.info("Opened accordian title section")
             accordian_content = self.element_is_visible(accordian[accordian_num]['content']).text
+            log.info(f"Get accordian content: {accordian_content}")
             accordian_title.click()
+            log.info("Closed accordian title section")
         except ElementClickInterceptedException:
             self.hide_ads()
             self.hide_image_ads()
             self.element_is_visible(accordian_title)
+            log.info("Accordian title is visible on the page")
             accordian_title.click()
+            log.info("Opened accordian title section")
             accordian_content = self.element_is_visible(accordian[accordian_num]['content']).text
+            log.info(f"Get accordian content: {accordian_content}")
             accordian_title.click()
+            log.info("Closed accordian title section")
+        log.info(f"Returned title, content: {accordian_title.text}\n{accordian_content}")
         return [accordian_title.text, accordian_content]
