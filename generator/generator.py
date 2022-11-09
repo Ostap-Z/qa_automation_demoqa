4571ba3e06231bfde84958e9576efb969f4b2b97
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-09 12:01:03 +0200

    Added generated_date that generates year, month, day, time with faker for QA needs

--- generator/generator.py
+++ generator/generator.py
@@ -2,9 +2,10 @@ from random import randint
 
 from faker import Faker
 
-from data.data import Person, Color
+from data.data import Person, Color, Date
 
 faker_ru = Faker("ru_RU")
+faker_en = Faker("En")
 Faker.seed()
 
 
@@ -34,4 +35,13 @@ def generated_file():
 def generated_color():
     yield Color(
         color_name=["Red", "Blue", "Green", "Yellow", "Purple", "Black", "White", "Voilet", "Indigo", "Magenta", "Aqua"]
+    )
+
+
+def generated_date():
+    yield Date(
+        year=faker_en.year(),
+        month=faker_en.month_name(),
+        day=faker_en.day_of_month(),
+        time="12:00"
     )
