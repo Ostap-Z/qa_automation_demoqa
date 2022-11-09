84799639c9d6f4dfc62fab8aa275d25eb295be32
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-09 17:28:24 +0200

    Added additional logs to the menu_page

--- pages/widgets_page/menu_page.py
+++ pages/widgets_page/menu_page.py
@@ -6,9 +6,12 @@ class MenuPage(BasePage):
     locators = MenuLocators()
 
     def check_menu(self):
+        log = self.get_logger()
         menu_item_list = self.elements_are_present(self.locators.MENU_ITEMS_LIST)
         data = []
         for item in menu_item_list:
             self.action_move_to_element(item)
             data.append(item.text)
+            log.info(f"Collected item: {item.text}")
+        log.info(f"Returned data: {data}")
         return data
