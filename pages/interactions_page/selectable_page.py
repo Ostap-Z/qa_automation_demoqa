0306c63902d060c3c2d359e7d115cc6eec851bf1
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-10 17:03:22 +0200

    Added additional logs to the selectable_page

--- pages/interactions_page/selectable_page.py
+++ pages/interactions_page/selectable_page.py
@@ -12,13 +12,21 @@ class SelectablePage(BasePage):
         sample(item_list, k=1)[0].click()
 
     def select_list_item(self):
+        log = self.get_logger()
         self.element_is_visible(self.locators.TAB_LIST).click()
+        log.info(f"Opened LIST tab")
         self.click_selectable_item(self.locators.LIST_ITEMS)
         active_element = self.element_is_visible(self.locators.LIST_ITEM_ACTIVE).text
+        log.info(f"Selected list item: {active_element}")
+        log.info(f"Length of selected list item: {len(active_element)}")
         return active_element
 
     def select_grid_item(self):
+        log = self.get_logger()
         self.element_is_visible(self.locators.TAB_GRID).click()
+        log.info(f"Opened GRID tab")
         self.click_selectable_item(self.locators.GRID_ITEMS)
         active_element = self.element_is_visible(self.locators.GRID_ITEM_ACTIVE).text
+        log.info(f"Selected grid item: {active_element}")
+        log.info(f"Length of selected grid item: {len(active_element)}")
         return active_element
