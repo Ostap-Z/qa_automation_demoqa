4d99012900ca4bdc6d0070bfb36e3ee2fcc54b22
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-10 16:57:29 +0200

    Added selectable locators for QA needs

---
+++ locators/interactions_locators/selectable_locators.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+
+
+class SelectableLocators:
+    TAB_LIST = (By.CSS_SELECTOR, "a#demo-tab-list")
+    LIST_ITEMS = (By.CSS_SELECTOR, "ul#verticalListContainer > li.mt-2.list-group-item.list-group-item-action")
+    LIST_ITEM_ACTIVE = (By.CSS_SELECTOR, "ul#verticalListContainer > li.list-group-item.active.list-group-item-action")
+
+    TAB_GRID = (By.CSS_SELECTOR, "a#demo-tab-grid")
+    GRID_ITEMS = (By.CSS_SELECTOR, "div#gridContainer li.list-group-item.list-group-item-action")
+    GRID_ITEM_ACTIVE = (By.CSS_SELECTOR, "div#gridContainer li.list-group-item.active.list-group-item-action")
