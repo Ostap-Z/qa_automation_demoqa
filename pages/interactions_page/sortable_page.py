aa7f4cde3c3db37c265753bb784178a99cf44079
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-11 12:43:59 +0200

    Added additional logs to the sortable_page

--- pages/interactions_page/sortable_page.py
+++ pages/interactions_page/sortable_page.py
@@ -12,6 +12,7 @@ class SortablePage(BasePage):
         return [item.text for item in item_list]
 
     def change_item_order(self, item_type):
+        log = self.get_logger()
         item = {
             'list': {
                 'tab': self.locators.TAB_LIST,
@@ -22,11 +23,15 @@ class SortablePage(BasePage):
                 'items': self.locators.GRID_ITEMS
             }
         }
-        self.element_is_visible(item[item_type]['tab']).click()
+        tab = self.element_is_visible(item[item_type]['tab'])
+        tab.click()
+        log.info(f"Opened tab: {tab.text}")
         order_before = self.get_sortable_items(item[item_type]['items'])
         item_list = sample(self.elements_are_visible(item[item_type]['items']), k=2)
         chosen_draggable_item = item_list[0]
         drag_to_position = item_list[1]
         self.action_drag_and_drop_to_element(chosen_draggable_item, drag_to_position)
+        log.info(f"{chosen_draggable_item.text} changed position with {drag_to_position.text}")
         order_after = self.get_sortable_items(item[item_type]['items'])
+        log.info(f"Returned before, after order: {order_before}, {order_after}")
         return order_before, order_after
