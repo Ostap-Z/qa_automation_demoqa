4c6d079af48a0c077e7d24d922a8b2282ed9ee34
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-15 13:06:30 +0200

    Added drop_accept method that drag and drop acceptable and not acceptable elements to target and returns their presented result text

--- pages/interactions_page/droppable_page.py
+++ pages/interactions_page/droppable_page.py
@@ -6,7 +6,19 @@ class DroppablePage(BasePage):
     locators = DroppableLocators()
 
     def drop_simple(self):
+        self.element_is_visible(self.locators.SIMPLE_TAB).click()
         drag_div = self.element_is_visible(self.locators.SIMPLE_DRAG_ME)
         drop_div = self.element_is_visible(self.locators.SIMPLE_DROP_HERE)
         self.action_drag_and_drop_to_element(drag_div, drop_div)
         return drop_div.text
+
+    def drop_accept(self):
+        self.element_is_visible(self.locators.ACCEPT_TAB).click()
+        acceptable_div = self.element_is_visible(self.locators.ACCEPT_ACCEPTABLE)
+        not_acceptable_div = self.element_is_visible(self.locators.ACCEPT_NOT_ACCEPTABLE)
+        drop_div = self.element_is_visible(self.locators.ACCEPT_DROP_HERE)
+        self.action_drag_and_drop_to_element(not_acceptable_div, drop_div)
+        drop_text_not_acceptable = drop_div.text
+        self.action_drag_and_drop_to_element(acceptable_div, drop_div)
+        drop_text_acceptable = drop_div.text
+        return drop_text_not_acceptable, drop_text_acceptable
