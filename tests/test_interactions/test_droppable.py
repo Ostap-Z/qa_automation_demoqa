cde4487686686933af44b200812e1fc5e3fc00e3
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-15 13:07:32 +0200

    Added test_accept_droppable that verifies actual result text equals to the expected

--- tests/test_interactions/test_droppable.py
+++ tests/test_interactions/test_droppable.py
@@ -11,3 +11,14 @@ class TestDroppable:
             f"\nActual result:\n\tItem has not been dropped.\n\tActual drop text: {drop_text_result}" \
             f"\nExpected result:\n\tActual drop text should be 'Dropped!'. So, the item was successfully dropped."
 
+    def test_accept_droppable(self, driver):
+        droppable_page = DroppablePage(driver, "https://demoqa.com/droppable")
+        droppable_page.open()
+        drop_text_not_acceptable, drop_text_acceptable = droppable_page.drop_accept()
+        assert drop_text_not_acceptable == "Drop here", \
+            f"\nActual result:\n\tActual not acceptable text result: {drop_text_not_acceptable}" \
+            f"\nExpected result:\n\tExpected not acceptable text result should be: Drop here"
+
+        assert drop_text_acceptable == "Dropped!", \
+            f"\nActual result:\n\tActual acceptable text result: {drop_text_acceptable}" \
+            f"\nExpected result:\n\tExpected acceptable text result should be: Dropped!"
