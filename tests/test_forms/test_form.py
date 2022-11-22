c185e4966aad81a1252e5250a21ad1a0048c2ecf
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-22 14:03:01 +0200

    Refactored form tests according to the PEP8

--- tests/test_forms/test_form.py
+++ tests/test_forms/test_form.py
@@ -6,10 +6,15 @@ from pages.forms_page.form_page import FormPage
 class TestForm:
 
     def test_form(self, driver):
-        form_page = FormPage(driver, "https://demoqa.com/automation-practice-form")
+        form_page = FormPage(
+            driver,
+            "https://demoqa.com/automation-practice-form"
+        )
         form_page.open()
         person = form_page.fill_form_fields()
         result = form_page.get_form_result()
-        assert [f"{person.first_name} {person.last_name}", person.email] == [result[0], result[1]], \
-            f"\nActual result: {result[0]} {result[1]}" \
-            f"\nExpected result: {person.first_name} {person.last_name} {person.email}"
+        assert [f"{person.first_name} {person.last_name}", person.email] \
+               == [result[0], result[1]], \
+               f"\nActual result: {result[0]} {result[1]}" \
+               f"\nExpected result: " \
+               f"{person.first_name} {person.last_name} {person.email}"
