89850ae77f2c642356b3df1af60956812cbeff45
Ostap-Z <zherebetskyiq7@gmail.com>
2022-11-22 12:35:26 +0200

    Refactored base page according to the PEP8

--- pages/base_page.py
+++ pages/base_page.py
@@ -19,25 +19,40 @@ class BasePage:
 
     def element_is_visible(self, locator, timeout=5):
         self.go_to_element(self.element_is_present(locator))
-        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+        return WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_element_located(locator)
+        )
 
     def elements_are_visible(self, locator, timeout=5):
-        return WebDriverWait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+        return WebDriverWait(self.driver, timeout).until(
+            EC.visibility_of_all_elements_located(locator)
+        )
 
     def element_is_present(self, locator, timeout=5):
-        return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+        return WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_element_located(locator)
+        )
 
     def elements_are_present(self, locator, timeout=5):
-        return WebDriverWait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+        return WebDriverWait(self.driver, timeout).until(
+            EC.presence_of_all_elements_located(locator)
+        )
 
     def element_is_not_visible(self, locator, timeout=5):
-        return WebDriverWait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
+        return WebDriverWait(self.driver, timeout).until(
+            EC.invisibility_of_element_located(locator)
+        )
 
     def element_is_clickable(self, locator, timeout=5):
-        return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+        return WebDriverWait(self.driver, timeout).until(
+            EC.element_to_be_clickable(locator)
+        )
 
     def go_to_element(self, locator):
-        self.driver.execute_script("arguments[0].scrollIntoView();", locator)
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView();",
+            locator
+        )
 
     def action_move_to_element(self, locator):
         action = ActionChains(self.driver)
@@ -54,22 +69,41 @@ class BasePage:
         action.context_click(locator)
         action.perform()
 
-    def action_drag_and_drop_by_offset(self, locator, x_coord, y_coord):
+    def action_drag_and_drop_by_offset(self,
+                                       locator,
+                                       x_coord,
+                                       y_coord):
         action = ActionChains(self.driver)
-        action.drag_and_drop_by_offset(locator, x_coord, y_coord)
+        action.drag_and_drop_by_offset(
+            locator,
+            x_coord,
+            y_coord
+        )
         action.perform()
 
-    def action_drag_and_drop_to_element(self, chosen_draggable_item, drag_to_position):
+    def action_drag_and_drop_to_element(self,
+                                        chosen_draggable_item,
+                                        drag_to_position):
         action = ActionChains(self.driver)
-        action.drag_and_drop(chosen_draggable_item, drag_to_position)
+        action.drag_and_drop(
+            chosen_draggable_item,
+            drag_to_position
+        )
         action.perform()
 
     def remove_footer(self):
-        self.driver.execute_script("document.getElementsByTagName('footer')[0].remove();")
-        self.driver.execute_script("document.getElementById('close-fixedban').remove();")
+        self.driver.execute_script(
+            "document.getElementsByTagName('footer')[0].remove();"
+        )
+        self.driver.execute_script(
+            "document.getElementById('close-fixedban').remove();"
+        )
 
     def hide_ads(self):
-        all_iframes = self.driver.find_elements(By.TAG_NAME, "iframe")
+        all_iframes = self.driver.find_elements(
+            By.TAG_NAME,
+            "iframe"
+        )
         if len(all_iframes) > 0:
             self.driver.execute_script("""
                 var elems = document.getElementsByTagName("iframe"); 
@@ -80,7 +114,10 @@ class BasePage:
                                   """)
 
     def hide_image_ads(self):
-        all_img_ads = self.driver.find_elements(By.TAG_NAME, "img")
+        all_img_ads = self.driver.find_elements(
+            By.TAG_NAME,
+            "img"
+        )
         if len(all_img_ads) > 0:
             self.driver.execute_script("""
                 var elems = document.getElementsByTagName("img"); 
@@ -111,7 +148,9 @@ class BasePage:
         logger_name = inspect.stack()[1][3]
         logger = logging.getLogger(logger_name)
         file_handler = logging.FileHandler('logfile.log')
-        formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
+        formatter = logging.Formatter(
+            "%(asctime)s : %(levelname)s : %(name)s : %(message)s"
+        )
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
         logger.setLevel(logging.DEBUG)
