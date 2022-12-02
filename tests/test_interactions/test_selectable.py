a3a24b1be059f8281ff87acf3806c08c968a317e
Ostap-Z <zherebetskyiq7@gmail.com>
2022-12-02 17:03:44 +0200

    Refactored tests

--- tests/test_interactions/test_selectable.py
+++ tests/test_interactions/test_selectable.py
@@ -9,28 +9,38 @@ class TestSelectable:
 
     @allure.title(
         "Verify that the user has an opportunity "
-        "to chose 'Selectable' elements"
+        "to chose the 'List' selectable elements"
     )
-    def test_selectable(self, driver):
+    def test_list_selectable(self, driver):
         selectable_page = SelectablePage(
             driver,
             "https://demoqa.com/selectable"
         )
         selectable_page.open()
         list_result = selectable_page.select_list_item()
-        grid_result = selectable_page.select_grid_item()
         assert len(list_result) > 0, \
             f"\nActual result:" \
             f"\n\tItem was not selected." \
             f"\n\tItem length: {list_result}" \
             f"\nExpected result:" \
             f"\n\tItem length should be more than 0. " \
-            f"So, item was selected."
+            f"\n\tSo, item was selected."
 
+    @allure.title(
+        "Verify that the user has an opportunity "
+        "to chose the 'Grid' selectable elements"
+    )
+    def test_grid_selectable(self, driver):
+        selectable_page = SelectablePage(
+            driver,
+            "https://demoqa.com/selectable"
+        )
+        selectable_page.open()
+        grid_result = selectable_page.select_grid_item()
         assert len(grid_result) > 0, \
             f"\nActual result:" \
             f"\n\tItem was not selected." \
             f"\n\tActual item length: {grid_result}" \
             f"\nExpected result:" \
             f"\n\tItem length should be more than 0. " \
-            f"So, item was selected."
+            f"\n\tSo, item was selected."
--- tests/test_interactions/test_sortable.py
+++ tests/test_interactions/test_sortable.py
@@ -11,7 +11,7 @@ class TestSortable:
         "Verify that the user has an opportunity "
         "to change an order of the 'Sortable' elements"
     )
-    def test_sortable(self, driver):
+    def test_list_sortable(self, driver):
         sortable_page = SortablePage(
             driver,
             "https://demoqa.com/sortable"
@@ -19,9 +19,6 @@ class TestSortable:
         sortable_page.open()
         list_order_before, list_order_after = \
             sortable_page.change_item_order("list")
-        grid_order_before, grid_order_after = \
-            sortable_page.change_item_order("grid")
-
         assert list_order_before != list_order_after, \
             f"\nActual result:" \
             f"\n\tSortable items were not changed." \
@@ -31,6 +28,20 @@ class TestSortable:
             f"\n\tSortable items order before and after " \
             f"should be different. So, items order was changed"
 
+
+
+    @allure.title(
+        "Verify that the user has an opportunity "
+        "to change an order of the 'Grid' sortable elements"
+    )
+    def test_grid_sortable(self, driver):
+        sortable_page = SortablePage(
+            driver,
+            "https://demoqa.com/sortable"
+        )
+        sortable_page.open()
+        grid_order_before, grid_order_after = \
+            sortable_page.change_item_order("grid")
         assert grid_order_before != grid_order_after, \
             f"\nActual result:" \
             f"\n\tSortable items were not changed." \
