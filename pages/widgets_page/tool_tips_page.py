8e8212a4f5f3ed25dcd6ebee2a3ee97a453ecf2f
Ostap-Z <zherebetskyiq7@gmail.com>
2022-12-02 18:38:43 +0200

    Refactored tests

--- pages/widgets_page/tool_tips_page.py
+++ pages/widgets_page/tool_tips_page.py
@@ -31,44 +31,46 @@ class ToolTipsPage(BasePage):
     @allure.step(
         "Check the different 'Tool Tips'"
     )
-    def check_tool_tips(self):
+    def check_tool_tips(self, tool_tip_type):
         log = self.get_logger()
 
         with allure.step("Check the 'Hover Me Tool Tip' button"):
-            tool_tip_text_button = \
-                self.get_tool_tips_text(
-                    self.locators.HOVER_BUTTON,
-                    self.locators.TOOL_TIP_BUTTON
-                )
-
-        log.info(f"Got tool tip text button: "
-                 f"{tool_tip_text_button}")
+            if tool_tip_type == "button":
+                tool_tip_text_button = \
+                    self.get_tool_tips_text(
+                        self.locators.HOVER_BUTTON,
+                        self.locators.TOOL_TIP_BUTTON
+                    )
+                log.info(f"Got tool tip text button: "
+                         f"{tool_tip_text_button}")
+                return tool_tip_text_button
 
         with allure.step("Check the 'Hover Me Tool Tip' input"):
-            tool_tip_text_input = self.get_tool_tips_text(
-                self.locators.HOVER_INPUT,
-                self.locators.TOOL_TIP_INPUT
-            )
-
-        log.info(f"Got tool tip text button: "
-                 f"{tool_tip_text_input}")
+            if tool_tip_type == "input":
+                tool_tip_text_input = self.get_tool_tips_text(
+                    self.locators.HOVER_INPUT,
+                    self.locators.TOOL_TIP_INPUT
+                )
+                log.info(f"Got tool tip text button: "
+                         f"{tool_tip_text_input}")
+                return tool_tip_text_input
 
         with allure.step("Check the 'Contrary Tool Tip' link"):
-            tool_tip_text_contrary = self.get_tool_tips_text(
-                self.locators.HOVER_CONTRARY_LINK,
-                self.locators.TOOL_TIP_CONTRARY
-            )
-
-        log.info(f"Got tool tip text button: "
-                 f"{tool_tip_text_contrary}")
+            if tool_tip_type == "contrary":
+                tool_tip_text_contrary = self.get_tool_tips_text(
+                    self.locators.HOVER_CONTRARY_LINK,
+                    self.locators.TOOL_TIP_CONTRARY
+                )
+                log.info(f"Got tool tip text button: "
+                         f"{tool_tip_text_contrary}")
+                return tool_tip_text_contrary
 
         with allure.step("Check the 'Section Tool Tip' link"):
-            tool_tip_text_section = self.get_tool_tips_text(
-                self.locators.HOVER_SECTION_LINK,
-                self.locators.TOOL_TIP_SECTION
-            )
-
-        log.info(f"Got tool tip text button: "
-                 f"{tool_tip_text_section}")
-        return tool_tip_text_button, tool_tip_text_input, \
-               tool_tip_text_contrary, tool_tip_text_section
+            if tool_tip_type == "section":
+                tool_tip_text_section = self.get_tool_tips_text(
+                    self.locators.HOVER_SECTION_LINK,
+                    self.locators.TOOL_TIP_SECTION
+                )
+                log.info(f"Got tool tip text button: "
+                         f"{tool_tip_text_section}")
+                return tool_tip_text_section
--- tests/test_widgets/test_tool_tips.py
+++ tests/test_widgets/test_tool_tips.py
@@ -9,44 +9,80 @@ class TestToolTips:
 
     @allure.title(
         "Verify that the user has an opportunity to "
-        "hover over the element and get a correct tool tip text"
+        "hover over the element 'Button' item "
+        "and get a correct tool tip text"
     )
-    def test_tool_tips(self, driver):
+    def test_button_tool_tip(self, driver):
         tool_tips_page = ToolTipsPage(
             driver,
             "https://demoqa.com/tool-tips"
         )
         tool_tips_page.open()
-        button_text, input_text, contrary_text, section_text = \
-            tool_tips_page.check_tool_tips()
-
+        button_text = tool_tips_page.check_tool_tips("button")
         assert button_text == "You hovered over the Button", \
             f"\nActual result:" \
             f"\n\tHovered text is empty or not equal to expected." \
             f"\n\tHovered text: {button_text}"\
-            F"\nExpected result:" \
-            F"\n\tHovered text should be: You hovered over the Button"
+            f"\nExpected result:" \
+            f"\n\tHovered text should be: " \
+            f"'You hovered over the Button'"
 
+    @allure.title(
+        "Verify that the user has an opportunity to "
+        "hover over the element 'Input' item "
+        "and get a correct tool tip text"
+    )
+    def test_input_tool_tip(self, driver):
+        tool_tips_page = ToolTipsPage(
+            driver,
+            "https://demoqa.com/tool-tips"
+        )
+        tool_tips_page.open()
+        input_text = tool_tips_page.check_tool_tips("input")
         assert input_text == "You hovered over the text field", \
             f"\nActual result:" \
             f"\n\tHovered text is empty or not equal to expected." \
             f"\n\tHovered text: {input_text}" \
-            F"\nExpected result:" \
-            F"\n\tHovered text should be: " \
-            F"You hovered over the text field"
+            f"\nExpected result:" \
+            f"\n\tHovered text should be: " \
+            f"'You hovered over the text field'"
 
+    @allure.title(
+        "Verify that the user has an opportunity to "
+        "hover over the element 'Contrary' item "
+        "and get a correct tool tip text"
+    )
+    def test_contrary_tool_tip(self, driver):
+        tool_tips_page = ToolTipsPage(
+            driver,
+            "https://demoqa.com/tool-tips"
+        )
+        tool_tips_page.open()
+        contrary_text = tool_tips_page.check_tool_tips("contrary")
         assert contrary_text == "You hovered over the Contrary", \
             f"\nActual result:" \
             f"\n\tHovered text is empty or not equal to expected." \
             f"\n\tHovered text: {contrary_text}" \
-            F"\nExpected result:" \
-            F"\n\tHovered text should be: " \
-            F"You hovered over the Contrary"
+            f"\nExpected result:" \
+            f"\n\tHovered text should be: " \
+            f"'You hovered over the Contrary'"
 
+    @allure.title(
+        "Verify that the user has an opportunity to "
+        "hover over the element 'Section' item "
+        "and get a correct tool tip text"
+    )
+    def test_section_tool_tip(self, driver):
+        tool_tips_page = ToolTipsPage(
+            driver,
+            "https://demoqa.com/tool-tips"
+        )
+        tool_tips_page.open()
+        section_text = tool_tips_page.check_tool_tips("section")
         assert section_text == "You hovered over the 1.10.32", \
             f"\nActual result:" \
             f"\n\tHovered text is empty or not equal to expected." \
             f"\n\tHovered text: {section_text}" \
             f"\nExpected result:" \
             f"\n\tHovered text should be: " \
-            f"You hovered over the 1.10.32"
+            f"'You hovered over the 1.10.32'"
